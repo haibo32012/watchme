2b8e067bd473ac35f8789a609e1f55de0823a4c9
haibo <haibo32012@gmail.com>
2017-03-25 20:34:16 +0800

    third commit

--- client/main.js
+++ client/main.js
@@ -1,22 +1,42 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import '../lib/collection.js';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.showVideo.onCreated(function () {
+  
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.showVideo.helpers({
+  file: function () {
+  	return files.findOne();
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.uploadForm.events({
+  'change #fileInput': function(e, template) {
+  	if (e.currentTarget.files && e.currentTarget.files[0]) {
+  		let upload = files.insert({
+  			file: e.currentTarget.files[0],
+  			streams: 'dynamic',
+  			chunkSize: 'dynamic'
+  		}, false);
+
+  		upload.on('start', function() {
+
+  		});
+
+  		upload.on('end', function(error, fileObj) {
+  			if (error) {
+  				alert('Error during upload: ' + error);
+  			} else {
+  				alert('File "' + fileObj.name + '" successfully uploaded');
+  			}
+
+  		});
+
+  		upload.start();
+  	}
+  }
 });
