03dbf42e4259bda671b5408893c87d1bf49ea33a
haibo <haibo32012@gmail.com>
2017-04-08 21:15:56 +0800

    eighth commit

--- client/main.js
+++ client/main.js
@@ -20,33 +20,4 @@ Template.showVideo.helpers({
   }
 });
 
-Template.uploadForm.events({
-  'change #fileInput': function(e, template) {
-  	if (e.currentTarget.files && e.currentTarget.files[0]) {
-      let video = e.currentTarget.files[0];
-      video.userId = Meteor.userId();
-  		let upload = files.insert({
 
-  			file: video,
-        //userId: Meteor.userId(),
-  			streams: 'dynamic',
-  			chunkSize: 'dynamic',
-      }, false);
-
-  		upload.on('start', function() {
-
-  		});
-
-  		upload.on('end', function(error, fileObj) {
-  			if (error) {
-  				alert('Error during upload: ' + error);
-  			} else {
-  				alert('File "' + fileObj.name + '" successfully uploaded');
-  			}
-
-  		});
-
-  		upload.start();
-  	}
-  }
-});
---
+++ client/uploadForm.js
@@ -0,0 +1,38 @@
+import {Meteor} from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import '../lib/collection.js';
+import '../imports/accounts-config.js';
+
+import './uploadForm.html'
+
+Template.uploadForm.events({
+  'change #fileInput': function(e, template) {
+  	if (e.currentTarget.files && e.currentTarget.files[0]) {
+      let video = e.currentTarget.files[0];
+      video.userId = Meteor.userId();
+  		let upload = files.insert({
+
+  			file: video,
+        //userId: Meteor.userId(),
+  			streams: 'dynamic',
+  			chunkSize: 'dynamic',
+      }, false);
+
+  		upload.on('start', function() {
+
+  		});
+
+  		upload.on('end', function(error, fileObj) {
+  			if (error) {
+  				alert('Error during upload: ' + error);
+  			} else {
+  				alert('File "' + fileObj.name + '" successfully uploaded');
+  			}
+
+  		});
+
+  		upload.start();
+  	}
+  }
+});
