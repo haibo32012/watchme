4eac5753b1f4083c56bdd1acc32a7b2901c10850
haibo <haibo32012@gmail.com>
2017-04-22 21:06:28 +0800

    twenty sixty commit

---
+++ client/subscribePage.js
@@ -0,0 +1,12 @@
+import {Meteor} from 'meteor/meteor';
+import './subscribePage.html';
+
+Template.subscribePage.helpers({
+	file: function() {
+		let userId = Meteor.userId();
+		let subscribeObject = subscribeCollection.find({userId: userId});
+		
+		console.log(subscribeObject.subscribedUserId);
+		return;
+	}
+});
