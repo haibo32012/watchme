2d1efd6a5574412a89e9383f89acd639b379f33f
haibo <haibo32012@gmail.com>
2017-06-30 15:44:28 +0800

    remove collection data update

--- client/userselfInformation/myVideoList.js
+++ client/userselfInformation/myVideoList.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import files from '/lib/collections/collection.js';
+import Comments from '/lib/collections/commentCollection.js';
+import shareCollection from '/lib/collections/sharecollection.js';
+import UserLikeCollection from '/lib/collections/userLikeCollection.js';
+import likeComment from '/lib/collections/userLikeComment.js';
+import userWatchedCollection from '/lib/collections/userWatchedCollection.js';
 import './myVideoList.html';
 
 Template.myVideoList.onCreated(function() {
@@ -19,11 +24,53 @@ Template.myVideoList.helpers({
 
 Template.myVideoList.events({
 	'click #deleteVideo': function() {
-		Comments.remove({videoId: this._id});
-		shareCollection.remove({videoId: this._id});
-		UserLikeCollection.remove({videoId: this._id});
-		likeComment.remove({videoId: this._id});
-		userWatchedCollection.remove({videoId: this._id});
-		files.remove(this._id);
+		//Comments.remove({videoId: this._id});
+		Meteor.call('commentRemove', {videoId: this._id}, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('comment remove!');
+			}
+		});
+		//shareCollection.remove({videoId: this._id});
+		Meteor.call('videoShareRemove', {videoId: this._id}, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('share collection remove success!');
+			}
+		});
+		//UserLikeCollection.remove({videoId: this._id});
+		Meteor.call('videoLikeRemove', {videoId: this._id}, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('video like collection data remove !');
+			}
+		});
+		//likeComment.remove({videoId: this._id});
+		Meteor.call('likeCommentObjectRemove', {videoId: this._id}, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('like comment remove ');
+			}
+		});
+		//userWatchedCollection.remove({videoId: this._id});
+		Meteor.call('userWatchedVideoRemove', {videoId: this._id}, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('user watched list release!');
+			}
+		});
+		//files.remove(this._id);
+		Meteor.call('videoRemove', this._id, (err) => {
+			if (err) {
+				alert(err);
+			} else {
+				console.log('video success remove!');
+			}
+		});
 	}
 });
